7e1983f3dcd6291b7c2bdd416a9523cb74a61670
Borys <undying4@gmail.com>
2024-05-24 09:17:20 +0200

    Added some views

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Shortly.Client.Models;
 using System.Diagnostics;
 
 namespace Shortly.Client.Controllers
@@ -18,15 +17,5 @@ namespace Shortly.Client.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ Shortly.Client/Controllers/UrlController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shortly.Client.Data.Models;
+
+namespace Shortly.Client.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Data is from DB
+            var urlDb = new Url()
+            {
+                Id = 1,
+                OriginalLink = "https://original.com",
+                ShortLink = "shortly",
+                NrOfClicks = 1,
+                UserId = 1,
+            };
+
+            return View(urlDb);
+        }
+    }
+}
